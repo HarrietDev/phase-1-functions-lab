57b2e0f0b367d99e5b131df6ae21b0677d406036
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-03-15 21:27:11 +0300

    Completed Scuber Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const hq = 42;
+return Math.abs( street - hq);
+}
+
+function distanceFromHqInFeet(street){
+    const blockLength = 264
+    return distanceFromHqInBlocks(street)*blockLength;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    const blockLength = 264
+return Math.abs(destination-start)*blockLength;
+}
+
+function calculatesFarePrice (start,destination){
+const distance= distanceTravelledInFeet(start, destination)
+if (distance <=400){
+    return 0;
+}
+ else if (distance>400 && distance<=2000){
+    return ( distance-400)*0.02;
+}
+  else if (distance>2000 &&  distance < 2500){
+    return 25;
+  }
+  else {
+    return "cannot travel that far";
+  }
+    
+}
+console.log( distanceFromHqInBlocks(43,44))
+console.log( distanceFromHqInBlocks(43,48))
+console.log(distanceFromHqInBlocks(50,58))
+console.log(distanceFromHqInBlocks(50,60))
+console.log(distanceFromHqInBlocks(34,32))
+console.log(distanceFromHqInBlocks(34,28))
+console.log(distanceFromHqInBlocks(34,24))
